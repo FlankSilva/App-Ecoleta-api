89d6c74a4573bdae1b6c8e6734e38754d8fbccad
FlankSilva <flank.silva.0@gmail.com>
2020-10-06 09:17:27 -0300

    Create table points

--- src/database/migrations/1601985710291-CreatePoints.ts
+++ src/database/migrations/1601985710291-CreatePoints.ts
@@ -1,11 +1,75 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export default class CreatePoints1601985710291 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+        await queryRunner.createTable(
+          new Table({
+            name: "points",
+            "columns": [
+              {
+                name: 'id',
+                type: 'uuid',
+                isPrimary: true,
+                generationStrategy: 'uuid',
+              },
+              {
+                name: 'image',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'name',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'email',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'whatsapp',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'latitude',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'longitude',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'city',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'uf',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'created_at',
+                type: 'timestamp',
+                default: 'now()',
+              },
+              {
+                name: 'update_at',
+                type: 'timestamp',
+                default: 'now()',
+              },
+            ]
+          })
+        )
+      }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('points')
     }
 
 }
