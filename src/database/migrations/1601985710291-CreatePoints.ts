c2bf5ac476074962ebd295aa59d4c4d3a0b6e39d
FlankSilva <flank.silva.0@gmail.com>
2020-10-06 09:04:30 -0300

    First commit

---
+++ src/database/migrations/1601985710291-CreatePoints.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export default class CreatePoints1601985710291 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
